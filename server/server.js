4317d43d117531e90cb7227ba632265a10e6a4da
susheel kumar <130265075+Susheeelk@users.noreply.github.com>
2025-10-05 14:30:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://swiggy-mern-frontend.onrender.com",
     credentials: true
 }))
 app.use(express.json())
