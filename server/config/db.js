88bbc27ff5faed7679f4799a43c47879ceb3a6ad
susheel kumar <130265075+Susheeelk@users.noreply.github.com>
2025-10-06 20:00:50 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const connectDb = async () => {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Database connected successfully.')
     } catch (error) {
-        console.log(error)
+        console.log(error.message)
     }
 }
 
